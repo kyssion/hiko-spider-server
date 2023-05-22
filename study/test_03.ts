a69e3613ddb43803a6c82a5c8fe1e49d314e9ee2
kyssion <1409915687@qq.com>
2023-05-22 20:38:57 +0800

    feat: z

---
+++ study/test_03.ts
@@ -0,0 +1,59 @@
+// ts 接口
+
+interface Person{
+    [propName : string] : any|number; // 指定对象的参数类型
+    name : string;
+    age? : number;
+}
+
+let one : Person = {
+    age: 0,
+    name: "",
+    fff: "123",
+    ffff:444,
+}
+
+interface Person1 {
+    name : string;
+    age: number;
+}
+
+interface Person2 {
+    name : string;
+    age: number;
+}
+
+// ts 使用鸭子类型 ， 只要相似就行
+
+function print(item : Person2){
+    console.log(item)
+}
+let t : Person1 = {
+    age: 0, name: ""
+}
+
+print(t)
+
+// 类型和别名 : todo 其实本身上是相似的， 但是interface 是一个新的类型, type 只是类型的别名
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface SetPoint {
+    (x: number, y: number): void;
+}
+
+type Point2 = {
+    x: number;
+    y: number;
+};
+
+type SetPoint2 = (x: number, y: number) => void;
+
+// 接口何以定义多次，
+interface Point { x: number; }
+interface Point { y: number; }
+const point: Point = { x: 1, y: 2 };
+
+
