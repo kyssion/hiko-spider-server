598bdfb1d4407f393a7880ee093060ae34ebde82
kys sion <kysskymail@gmail.com>
2023-05-25 00:41:44 +0800

    feat: 修改数据

--- study/test_03.ts
+++ study/test_03.ts
@@ -28,9 +28,21 @@ interface Person2 {
 function print(item : Person2){
     console.log(item)
 }
+
+class XXX {
+    name : string = ""
+}
+
+interface Interface {
+    name : string
+    age : string
+}
+
 let t : Person1 = {
     age: 0, name: ""
 }
+
+print({age: 0, name: ""})
 
 print(t)
 
@@ -55,5 +67,6 @@ type SetPoint2 = (x: number, y: number) => void;
 interface Point { x: number; }
 interface Point { y: number; }
 const point: Point = { x: 1, y: 2 };
+
 
 
