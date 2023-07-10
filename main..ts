16a24a45913db057ab1742e6a33b31f4433a4f39
kys sion <kysskymail@gmail.com>
2023-07-10 22:34:32 +0800

    feat: 增加http支持

---
+++ main..ts
@@ -0,0 +1,23 @@
+import * as http from "http";
+import * as https from "https"
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+
+https.get('https://www.baidu.com/', (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
